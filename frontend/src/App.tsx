179597e3caa06eb62cb8bec88c7ba7c3c2789e4a
Susanna Andreeva <sanadex90@gmail.com>
2024-02-08 21:52:24 +0900

    add style for App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
   const [originalUrl, setOriginalUrl] = useState('');
   const [shortenedUrl, setShortenedUrl] = useState('');
 
-  const shortUrl = async () => {
+  const shortUrl = async (e: React.FormEvent) => {
+    e.preventDefault();
     try {
       const response = await axios.post('http://localhost:8000/links', {url: originalUrl});
       setShortenedUrl(response.data.shortUrl);
@@ -15,18 +16,21 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center mt-3">
       <h1>Shorten your link</h1>
-      <input
-        type="text"
-        placeholder="Enter URL"
-        value={originalUrl}
-        onChange={(e) => setOriginalUrl(e.target.value)}
-      />
-      <button onClick={shortUrl}>Shorten</button>
+      <form className="input-group mb-3 container-fluid" onSubmit={shortUrl}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter URL"
+          value={originalUrl}
+          onChange={(e) => setOriginalUrl(e.target.value)}
+        />
+        <button className="btn btn-outline-secondary" type="submit">Shorten!</button>
+      </form>
       {shortenedUrl && (
-        <div>
-          <p>Shortened URL:</p>
+        <div className="d-flex flex-column align-items-center">
+          <h3>Your link now looks like this</h3>
           <a href={`http://localhost:8000/${shortenedUrl}`} target="_blank">
             {`http://localhost:8000/${shortenedUrl}`}
           </a>
